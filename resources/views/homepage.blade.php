415146a2e03a07eaa42a84a09e29254ef01ad575
David Paparo <d.paparo@tiscali.it>
2025-11-07 16:07:38 +0100

    restyling grafico

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,71 +4,81 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FaiDaTe</title>
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+
+    {{-- css Custom --}}
+    <link rel="stylesheet" href="style.css">
 
 
     {{-- Google font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Stack+Sans+Notch:wght@200..700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
 
-    {{-- CSS custom --}}
-    <link rel="stylesheet" href="/style.css">
+
 
 </head>
 
-{{-- 13 self --}}
-{{-- 34:43 front --}}
 
 <body>
 
 
 
+    {{-- titolo --}}
+
+    <div class="container-fluid bianco box_1">
+
+        <div class="row h-100">
+            <div class="col-12 d-flex justify-content-center align-items-center flex-column">
+                <h1 class="titolo">Crafted Studio</h1>
+                <p class="slogan">Crea valore con le tue mani</p>
+            </div>
+        </div>
+
+    </div>
 
 
-    <nav class="navbar navbar-expand-md fixed-top">
+    {{-- navbar --}}
+
+    <nav class="navbar navbar-expand-lg beige">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              <img src="/media/logo black.png" alt="logo sito" class="logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto">
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                        <a class="nav-link" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item nav-item-center">
+                        <a class="nav-link" href="#">ARTICOLI</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/articoli">articoli</a>
+                        <a class="nav-link" href="#">ABOUT US</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">chi siamo</a>
-                    </li>
+
+
                 </ul>
             </div>
         </div>
     </nav>
 
-    <header>
+    {{-- fine navbar --}}
 
-        <div class="container-fluid css-header">
-            <div class="row h-100">
-                <div class="col-12 d-flex justify-content-center align-items-end">
-                    <h1 class="textFirst">Fai da Te blog</h1>
-                </div>
-                <div class="row ">
-                <div class="col-12 d-flex justify-content-center align-items-start">
-                    <p class="textSecond">...chi fa da se... fà perchè..</p>
-                </div>
-                </div>
-            </div>
 
-    </header>
+
+
+
+
 
 
 
